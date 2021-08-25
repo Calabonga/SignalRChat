b6cb6571a6cb2e34da0f336844683fa8c047572e
Calabonga <dev@calabonga.net>
2021-08-25 13:11:31 +0700

    WPF Client empty project with some preparations

---
+++ Calabonga.Chat.WpfClient/Calabonga.Chat.WpfClient/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Calabonga.Chat.WpfClient.Views;
+using Prism.Ioc;
+
+namespace Calabonga.Chat.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        /// <summary>
+        /// Used to register types with the container that will be used by your application.
+        /// </summary>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        /// <summary>Creates the shell or main window of the application.</summary>
+        /// <returns>The shell of the application.</returns>
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+    }
+}
---
+++ Calabonga.Chat.WpfClient/Calabonga.Chat.WpfClient/ViewModels/ShellViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+
+namespace Calabonga.Chat.WpfClient.ViewModels
+{
+    /// <summary>
+    /// Shell ViewModel
+    /// </summary>
+    public class ShellViewModel : BindableBase {
+ 
+        #region property DisplayName
+ 
+        /// <summary>
+        /// Represent DisplayName property
+        /// </summary>
+        public string DisplayName {
+            get => _displayName;
+            set => SetProperty(ref _displayName, value);
+        }
+ 
+        /// <summary>
+        /// Backing field for property DisplayName
+        /// </summary>
+        private string _displayName = "WPF Chat Client";
+ 
+        #endregion
+ 
+    }
+}
